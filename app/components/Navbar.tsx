519a35d2742f6d84247ba56a4cf90a74b1985bfb
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-06-04 18:18:59 -0400

    creates basic navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-800 flex font-bold text-2xl py-3 justify-between items-center">
+        <div className="mx-4 cursor-pointer">
+            <Link href='/'>
+                My Groceries
+            </Link>
+        </div>
+        <div className="mx-4 cursor-pointer">
+            <Link href='/recipes'>
+                Search Recipes
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
+
+

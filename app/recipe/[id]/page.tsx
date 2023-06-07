ba94518cb92b1cf5ae6737bd1d6f023dfaffafaf
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-06-07 17:55:04 -0400

    feature: api call for recipe details

--- app/recipe/[id]/page.tsx
+++ app/recipe/[id]/page.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
+import { RecipeType } from "@/types";
 
-const Page = () => {
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+const Page: React.FC<Params> = ({ params }) => {
+  const [recipe, setRecipe] = useState<Partial<RecipeType>>({});
+
+  useEffect(() => {
+    const fetchRecipeInfo = async () => {
+      try {
+        const response = await fetch('/api/recipe-info', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id: params.id }),
+        });
+        if (!response.ok) {
+          throw new Error('Response not ok');
+        }
+        const data = await response.json()
+        setRecipe(data.recipeInfo);
+      } catch (error) {
+        console.error('Error fetching recipe: ', error);
+      }
+    }
+    fetchRecipeInfo();
+  });
+
   return (
-    <div>Page</div>
+    <div>
+        { recipe ? (
+          <div>
+            <h1>{recipe.title}</h1>
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+    </div>
   )
 }
 
